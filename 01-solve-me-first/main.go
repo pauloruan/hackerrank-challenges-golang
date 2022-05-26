73c0486811e1234f88bdbfcc92f4259861c34f83
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-05-26 16:11:12 -0300

    feat: Challenger "Solve Me First" concluded.

---
+++ 01-solve-me-first/main.go
@@ -0,0 +1,15 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+// Example:  a = 4, b = 7, return 11.
+
+package main
+
+import "fmt"
+
+func solveMeFirst(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	var result = solveMeFirst(13, 13)
+	fmt.Println(result)
+}
